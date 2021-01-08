0533dcd0564f2cc4b9a410b1647c713d3f3dd1f5
wajuma <meltusjuma5@gmail.com>
2021-01-08 12:52:06 +0300

    score

--- app.py
+++ app.py
@@ -34,3 +34,22 @@ snake.color('black')
 snake.penup()
 snake.goto(0,0)
 snake.direction ='stop'
+
+# fruit (rodents of course)
+fruit = turtle.Turtle()
+fruit.speed(0)
+fruit.shape('circle')
+fruit.color('red')
+fruit.penup()
+fruit.goto(30,30)
+
+#score
+old_fruit=[]
+
+scoring = turtle.Turtle()
+scoring.speed(0)
+scoring.color("black")
+scoring.penup()
+scoring.hideturtle()
+scoring.goto(0,300)
+scoring.write("Score :",align="center",font=("Courier",24,"bold")
