4f9b21c47cf4e989abf09da82a7d2bcb18c7c329
wajuma <meltusjuma5@gmail.com>
2021-01-08 12:30:35 +0300

    set up screen

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import turtle
 import time
 import random
+
+# moms farm
+
+farm =turtle.Screen()
+farm.title("Snake Game")
+farm.setup(width=700, height=700) # not as small though
+farm.tracer(0)
+farm.bgcolor('green')
